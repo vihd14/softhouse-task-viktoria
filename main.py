e0f92ebba5956733d76fb83d9232e39c36703625
Viktoria Haapaoja <viktoria.haa@hotmail.com>
2019-04-23 21:57:52 +0200

    Finished first draft of the working script

--- main.py
+++ main.py
@@ -1,13 +1,90 @@
+import re
 import xml.etree.cElementTree as ET
+import xml.dom.minidom
 
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
+people_tag = ET.Element("people")
 
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+with open("file.txt") as textfile:
+    textlines = textfile.readlines()
 
-tree = ET.ElementTree(root)
-tree.write("file_xml.xml")
+    for textline in textfile:
+        textlines.append(textline.rstrip('\n'))
 
-f = open("file.txt", "r")
-print(f.readline())
+    prevline = ""
+
+    for line in textlines:
+        if line[0] == "P":
+            firstname = re.search(r'\|.*\|', line)
+            lastname = re.search(r'\|.*', line)
+            firstname = firstname[0].split('|')[1]
+            lastname = lastname[0].split('|')[2]
+
+            person_tag = ET.SubElement(people_tag, "person")
+            ET.SubElement(person_tag, "firstname").text = firstname
+            ET.SubElement(person_tag, "lastname").text = lastname
+
+        elif line[0] == "A" and prevline[0] != "F":
+            address_info = re.search(r'\|.*\|', line)
+            street = address_info[0].split('|')[1]
+            city = address_info[0].split('|')[2]
+            postcode = re.search(r'\|.*', line)
+            postcode = postcode[0].split('|')[3]
+            print("City: ", city)
+
+            address = ET.SubElement(person_tag, "address")
+            ET.SubElement(address, "street").text = street
+            ET.SubElement(address, "city").text = city
+            ET.SubElement(address, "postcode").text = postcode
+
+        elif line[0] == "T" and prevline[0] != "F":
+            mobile = re.search(r'\|.*\|', line)
+            landline = re.search(r'\|.*', line)
+            mobile = mobile[0].split('|')[1]
+            landline = landline[0].split('|')[2]
+
+            phone = ET.SubElement(person_tag, "phone")
+            mobile = ET.SubElement(phone, "mobile").text = mobile
+            landline = ET.SubElement(phone, "landline").text = landline
+
+        elif line[0] == "F":
+            name = re.search(r'\|.*\|', line)
+            name = name[0].split('|')[1]
+            born = re.search(r'\|.*', line)
+            born = born[0].split('|')[2]
+
+            family = ET.SubElement(person_tag, "family")
+            ET.SubElement(family, "name").text = name
+            ET.SubElement(family, "born").text = born
+
+        elif line[0] == "A" and prevline[0] == "F":
+            address_info = re.search(r'\|.*\|', line)
+            street = address_info[0].split('|')[1]
+            city = address_info[0].split('|')[2]
+            postcode = re.search(r'\|.*', line)
+            postcode = postcode[0].split('|')[3]
+
+            address = ET.SubElement(family, "address")
+            ET.SubElement(address, "street").text = street
+            ET.SubElement(address, "city").text = city
+            ET.SubElement(address, "postcode").text = postcode
+        
+        elif line[0] == "T" and prevline[0] == "F":
+            mobile = re.search(r'\|.*\|', line)
+            landline = re.search(r'\|.*', line)
+            mobile = mobile[0].split('|')[1]
+            landline = landline[0].split('|')[2]
+
+            phone = ET.SubElement(family, "phone")
+            mobile = ET.SubElement(phone, "mobile").text = mobile
+            landline = ET.SubElement(phone, "landline").text = landline
+        
+        prevline = line
+
+    tree = ET.ElementTree(people_tag)
+    tree.write("file_xml.xml")
+
+    with open('file_xml.xml') as xmldata:
+        xml = xml.dom.minidom.parseString(xmldata.read())
+        xml_pretty_str = xml.toprettyxml()
+
+    print(xml_pretty_str)
